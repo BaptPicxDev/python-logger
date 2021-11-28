b9f7f0905d44367ab74d2587c431d511a7322b72
Baptiste <picard.baptiste@laposte.net>
2021-11-28 16:31:10 +0100

    Cleaning and updating

--- source/logger.py
+++ source/logger.py
@@ -3,12 +3,14 @@
     Date: 27/11/2021
     Contact: picard.baptiste22@gmail.com
 
-    Creating a logger
+    Creating a logger base on basicConfig and specific handlers.
 """
 
 # Imports
 from logging import (
-    basicConfig,
+    getLogger,
+    Formatter,
+    StreamHandler,
     DEBUG,
     INFO,
     WARNING,
@@ -20,10 +22,106 @@ from logging.handlers import TimedRotatingFileHandler
 # Environment
 
 
+class HomemadeLogger:
+    """ Homemade logger. """
+    def __init__(self, name="My Own Logger",
+                 level=INFO,
+                 log_format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                 handlers=[]):
+        """
+            Init function.
+        """
+        if not isinstance(name, str):
+            raise TypeError(f"Name must be str instead of {type(name)}.")
+        if not isinstance(level, int):
+            raise TypeError(f"Level must be str instead of {type(level)}.")
+        if not isinstance(log_format, str):
+            raise TypeError(f"Log_format must be str instead of {type(log_format)}.")
+        if not isinstance(handlers, list):
+            raise TypeError(f"Handlers must be list instead of {type(handlers)}.")
+        if level not in [DEBUG,
+                         INFO,
+                         WARNING,
+                         ERROR]:
+            raise ValueError(f"Level must be in [DEBUG, INFO, WARNING, ERROR] instead of {level}.")
+        self.name = (name.strip().lower()
+                     .replace(' ', '_'))
+        self.level = level
+        self.format = log_format
+        self.handlers = handlers
+        self.logger = self.create_logger()
+
+    def get_name(self):
+        """
+        Get the logger name.
+
+        :return: str - Logger name
+        """
+        return self.name
+
+    def get_level(self):
+        """
+        Get the logger level.
+
+        :return: int - the logging level
+        """
+        return self.level
+
+    def get_format(self):
+        """
+        Get the logger format.
+
+        :return: str - the logging format
+        """
+        return self.format
+
+    def get_handlers(self):
+        """
+        Get the logger handlers.
+
+        :return: list
+        """
+        return self.handlers
+
+    def get_logger(self):
+        """
+        Get the logger object.
+
+        :return: <Logger> - the logger
+        """
+        return self.logger
+
+    def create_logger(self):
+        """ Create the logger object using the init config. """
+        logger = getLogger(self.get_name())
+        logger.setLevel(self.get_level())
+        handler = StreamHandler()
+        handler.setLevel(self.get_level())
+        handler.setFormatter(Formatter(self.get_format()))
+        logger.addHandler(handler)
+        return logger
+    #
+    # def log(self, message):
+    #     """ Log a specific message to the logger with INFO level. """
+    #     self.get_logger().log(INFO, message)
+    #
+    # def debug(self, message):
+    #     """ Log a specific message to the logger with DEBUG level. """
+    #     self.get_logger().log(DEBUG, message)
+    #
+    # def warning(self, message):
+    #     """ Log a specific message to the logger with WARNING level. """
+    #     self.get_logger().log(WARNING, message)
+    #
+    # def error(self, message):
+    #     """ Log a specific message to the logger with ERROR level. """
+    #     self.get_logger().log(ERROR, message)
+
+
 class HomemadeTimedRotatingFileHandler:
     """ Homemade TimedRotatingFileHandler. """
     def __init__(self, filename='log', when='h', interval=1,
-                 log_format="%(asctime)s %(message)s"):
+                 log_format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"):
         """
             Init function.
         """
@@ -33,12 +131,14 @@ class HomemadeTimedRotatingFileHandler:
             raise TypeError(f"When must be str instead of {type(when)}.")
         if not isinstance(interval, int):
             raise TypeError(f"Interval must be int instead of {type(interval)}.")
-        if when.upper() not in ['S', 'M', 'H', 'D',
-                                'W0', 'W1', 'W2', 'W3',
-                                'W4', 'W5', 'W6', 'midnight']:
-            raise ValueError("Bad value for when.")
         if not isinstance(log_format, str):
             raise TypeError(f"log_format must be str instead of {type(log_format)}.")
+        if when.upper() not in ['S', 'M', 'H', 'D',
+                                'W0', 'W1', 'W2', 'W3',
+                                'W4', 'W5', 'W6', 'MIDNIGHT']:
+            raise ValueError("Bad value for when.")
+        if interval < 1:
+            raise ValueError("Interval must be >=1")
         self.filename = filename
         self.when = when.upper()
         self.interval = interval
@@ -81,66 +181,3 @@ class HomemadeTimedRotatingFileHandler:
         return TimedRotatingFileHandler(filename=self.get_filename(),
                                         when=self.get_when(),
                                         interval=self.get_interval())
-
-
-class HomemadeLogger:
-    """ Homemade logger. """
-    def __init__(self, name="My Own Logger",
-                 level=INFO,
-                 log_format="%(asctime)s %(message)s"):
-        """
-            Init function.
-        """
-        if not isinstance(name, str):
-            raise TypeError(f"Name must be str instead of {type(name)}.")
-        if not isinstance(level, int):
-            raise TypeError(f"Level must be str instead of {type(level)}.")
-        if level not in [DEBUG,
-                         INFO,
-                         WARNING,
-                         ERROR]:
-            raise ValueError(f"Level must be in [DEBUG, INFO, WARNING, ERROR] instead of {level}.")
-        if not isinstance(log_format, str):
-            raise TypeError(f"log_format must be str instead of {type(log_format)}.")
-        self.name = name
-        self.level = level
-        self.format = log_format
-        self.logger = self.create_logger()
-
-    def get_name(self):
-        """
-        Get the logger name.
-
-        :return: str - Logger name
-        """
-        return self.name
-
-    def get_level(self):
-        """
-        Get the logger level.
-
-        :return: int - the logging level
-        """
-        return self.level
-
-    def get_format(self):
-        """
-        Get the logger format.
-
-        :return: str - the logging format
-        """
-        return self.format
-
-    def get_logger(self):
-        """
-        Get the logger object.
-
-        :return: <Logger> - the logger
-        """
-        return self.logger
-
-    def create_logger(self):
-        """ Create the logger object using the init config. """
-        return basicConfig(filename=self.get_name(),
-                           level=self.get_level(),
-                           format=self.get_format())
--- tests/test_logger.py
+++ tests/test_logger.py
@@ -7,7 +7,6 @@
 """
 
 # Imports.
-import sys
 from os import remove
 from os.path import (
     join,
@@ -15,14 +14,16 @@ from os.path import (
     exists
 )
 import pytest
+from unittest import TestCase
 
 # Environment
+import sys
 sys.path.append(join(abspath('.'), 'source'))
 
 # Project modules.
+from logging import Logger
 from logger import (
     HomemadeLogger,
-    basicConfig,
     HomemadeTimedRotatingFileHandler,
     TimedRotatingFileHandler,
     INFO,
@@ -31,14 +32,21 @@ from logger import (
 )
 
 
-class TestHomemadeLogger:
+class TestHomemadeLogger(TestCase):
     """
         Test the class HomemadeLogger.
     """
 
     def setup_class(self):
         """ Setup function called before each test. """
-        pass
+        self.test_logger_name = 'My new logger'
+        self.test_logger_name_1 = 'My new logger 1'
+        self.test_logger_level = ERROR
+        self.test_logger_level_1 = DEBUG
+        self.test_logger_format = "%(asctime)s - %(name)s - %(levelname)s"
+        self.test_logger_format_1 = "%(asctime)s - %(name)s"
+        self.test_message = 'How'
+        self.test_message_1 = 'Are'
 
     def teardown_class(self):
         """ Setup function called before each test. """
@@ -54,11 +62,16 @@ class TestHomemadeLogger:
             HomemadeLogger(name=True)
             HomemadeLogger(name=[])
             HomemadeLogger(name={})
-        logger_name = 'My new logger'
         logger = HomemadeLogger()
-        assert logger.get_name() == 'My Own Logger'
-        logger = HomemadeLogger(name=logger_name)
-        assert logger.get_name() == logger_name
+        assert logger.get_name() == 'my_own_logger'
+        logger = HomemadeLogger(name=self.test_logger_name)
+        assert logger.get_name() == (self.test_logger_name
+                                     .strip().lower()
+                                     .replace(' ', '_'))
+        logger = HomemadeLogger(name=self.test_logger_name_1)
+        assert logger.get_name() == (self.test_logger_name_1
+                                     .strip().lower()
+                                     .replace(' ', '_'))
 
     def test_get_level(self):
         """ Test the HomemadeLogger.get_level function. """
@@ -71,11 +84,12 @@ class TestHomemadeLogger:
             HomemadeLogger(level=1)
             HomemadeLogger(level=11)
             HomemadeLogger(level=42)
-        logger_level = ERROR
         logger = HomemadeLogger()
         assert logger.get_level() == INFO
-        logger = HomemadeLogger(level=logger_level)
-        assert logger.get_level() == ERROR
+        logger = HomemadeLogger(level=self.test_logger_level)
+        assert logger.get_level() == self.test_logger_level
+        logger = HomemadeLogger(level=self.test_logger_level_1)
+        assert logger.get_level() == self.test_logger_level_1
 
     def test_get_format(self):
         """ Test the HomemadeLogger.get_format function. """
@@ -84,32 +98,59 @@ class TestHomemadeLogger:
             HomemadeLogger(name=True)
             HomemadeLogger(name=[])
             HomemadeLogger(name={})
-        logger_format = "my new format"
         logger = HomemadeLogger()
-        assert logger.get_format() == "%(asctime)s %(message)s"
-        logger = HomemadeLogger(log_format=logger_format)
-        assert logger.get_format() == logger_format
+        assert logger.get_format() == "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        logger = HomemadeLogger(log_format=self.test_logger_format)
+        assert logger.get_format() == self.test_logger_format
+        logger = HomemadeLogger(log_format=self.test_logger_format_1)
+        assert logger.get_format() == self.test_logger_format_1
 
     def test_get_logger(self):
         """ Test the HomemadeLogger.get_logger function. """
+        homemade_logger = HomemadeLogger(name='This is a test',
+                                         level=DEBUG,
+                                         log_format='test').get_logger()
+        assert homemade_logger.hasHandlers()
+        assert isinstance(homemade_logger, Logger)
+
+    def test_init(self):
+        """ Test the HomemadeLogger.__init__ function. """
         with pytest.raises(TypeError):
             HomemadeLogger(name=1,
                            level=INFO,
-                           log_format='format')
+                           log_format='format',
+                           handllers=[])
             HomemadeLogger(name='Test logger',
                            level=[INFO, DEBUG],
-                           log_format='format')
+                           log_format='format',
+                           handllers=[])
             HomemadeLogger(name='Test logger',
                            level=DEBUG,
-                           log_format={'format': 1})
+                           log_format={'format': 1},
+                           handllers=[])
+            HomemadeLogger(name='Test logger',
+                           level=DEBUG,
+                           log_format='format',
+                           handllers={})
+            HomemadeLogger(name='Test logger',
+                           level=DEBUG,
+                           log_format='format',
+                           handllers=TimedRotatingFileHandler)
         with pytest.raises(ValueError):
             HomemadeLogger(name='This is a test',
                            level=42,
                            log_format='test')
-        homemade_logger = HomemadeLogger(name='This is a test',
-                                         level=DEBUG,
-                                         log_format='test')
-        assert isinstance(homemade_logger.get_logger(), type(basicConfig()))
+
+    def test_logging(self):
+        logger = HomemadeLogger().get_logger()
+        with self.assertLogs() as captured:
+            logger.log(INFO, self.test_message)
+            logger.log(ERROR, self.test_message_1)
+        assert len(captured.records) == 2
+        assert captured.records[0].getMessage() == self.test_message
+        assert captured.records[0].levelname == 'INFO'
+        assert captured.records[1].getMessage() == self.test_message_1
+        assert captured.records[1].levelname == 'ERROR'
 
 
 class TestHomemadeTimedRotatingFileHandler:
@@ -119,14 +160,21 @@ class TestHomemadeTimedRotatingFileHandler:
 
     def setup_class(self):
         """ Setup function called before each test. """
-        pass
+        self.test_filename = 'test_log'
+        self.test_filename_1 = 'test_log_1'
+        self.test_when = 'w2'
+        self.test_when_1 = 'Midnight'
+        self.test_interval = 1
+        self.test_interval_1 = 100
 
     def teardown_class(self):
         """ Setup function called before each test. """
         if exists('log'):
             remove('log')
-        if exists('test_log'):
-            remove('test_log')
+        if exists(self.test_filename):
+            remove(self.test_filename)
+        if exists(self.test_filename_1):
+            remove(self.test_filename_1)
 
     def test_get_name(self):
         """ Test the HomemadeTimedRotatingFileHandler.get_name function. """
@@ -135,11 +183,12 @@ class TestHomemadeTimedRotatingFileHandler:
             HomemadeTimedRotatingFileHandler(filename=True)
             HomemadeTimedRotatingFileHandler(filename=[])
             HomemadeTimedRotatingFileHandler(filename={})
-        test_filename = 'test_log'
         handler = HomemadeTimedRotatingFileHandler()
         assert handler.get_filename() == 'log'
-        handler = HomemadeTimedRotatingFileHandler(filename=test_filename)
-        assert handler.get_filename() == test_filename
+        handler = HomemadeTimedRotatingFileHandler(filename=self.test_filename)
+        assert handler.get_filename() == self.test_filename
+        handler = HomemadeTimedRotatingFileHandler(filename=self.test_filename_1)
+        assert handler.get_filename() == self.test_filename_1
 
     def test_get_when(self):
         """ Test the HomemadeTimedRotatingFileHandler.get_when function. """
@@ -151,27 +200,39 @@ class TestHomemadeTimedRotatingFileHandler:
         with pytest.raises(ValueError):
             HomemadeTimedRotatingFileHandler(when='hello')
             HomemadeTimedRotatingFileHandler(when='SS')
-        test_when = 'w2'
         handler = HomemadeTimedRotatingFileHandler()
         assert handler.get_when() == 'H'
-        handler = HomemadeTimedRotatingFileHandler(when=test_when)
-        assert handler.get_when() == test_when.upper()
+        handler = HomemadeTimedRotatingFileHandler(when=self.test_when)
+        assert handler.get_when() == self.test_when.upper()
+        handler = HomemadeTimedRotatingFileHandler(when=self.test_when_1)
+        assert handler.get_when() == self.test_when_1.upper()
 
     def test_get_interval(self):
         """ Test the HomemadeTimedRotatingFileHandler.get_interval function. """
         with pytest.raises(TypeError):
             HomemadeTimedRotatingFileHandler(interval='')
-            HomemadeTimedRotatingFileHandler(when=True)
-            HomemadeTimedRotatingFileHandler(when=[])
-            HomemadeTimedRotatingFileHandler(when={})
-        test_interval = 1
+            HomemadeTimedRotatingFileHandler(interval=True)
+            HomemadeTimedRotatingFileHandler(interval=[])
+            HomemadeTimedRotatingFileHandler(interval=[100])
+            HomemadeTimedRotatingFileHandler(interval={})
+        with pytest.raises(ValueError):
+            HomemadeTimedRotatingFileHandler(interval=-1)
+            HomemadeTimedRotatingFileHandler(interval=0)
         handler = HomemadeTimedRotatingFileHandler()
         assert handler.get_interval() == 1
-        handler = HomemadeTimedRotatingFileHandler(interval=test_interval)
-        assert handler.get_interval() == test_interval
+        handler = HomemadeTimedRotatingFileHandler(interval=self.test_interval)
+        assert handler.get_interval() == self.test_interval
+        handler = HomemadeTimedRotatingFileHandler(interval=self.test_interval_1)
+        assert handler.get_interval() == self.test_interval_1
 
     def test_get_handler(self):
         """ Test the HomemadeTimedRotatingFileHandler.get_handler function. """
+        handler = HomemadeTimedRotatingFileHandler()
+        assert isinstance(handler.get_handler(),
+                          type(TimedRotatingFileHandler(filename='log')))
+
+    def test_init(self):
+        """ Test the HomemadeTimedRotatingFileHandler.__init__ function. """
         with pytest.raises(TypeError):
             HomemadeTimedRotatingFileHandler(filename=1,
                                              when='S or H',
@@ -194,6 +255,7 @@ class TestHomemadeTimedRotatingFileHandler:
                                              when='Minute',
                                              interval=1,
                                              log_format='format')
-        handler = HomemadeTimedRotatingFileHandler()
-        assert isinstance(handler.get_handler(),
-                          type(TimedRotatingFileHandler(filename='log')))
+            HomemadeTimedRotatingFileHandler(filename='test filename',
+                                             when='Minute',
+                                             interval=-42,
+                                             log_format='format')
